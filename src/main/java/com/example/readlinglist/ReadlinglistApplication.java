006cc3a8d1403a21aa5cf754eba8a36ef84085a9
zxy <orcexzy@163.com>
2019-03-07 18:02:01 +0800

    first commit

---
+++ src/main/java/com/example/readlinglist/ReadlinglistApplication.java
@@ -0,0 +1,13 @@
+package com.example.readlinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadlinglistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadlinglistApplication.class, args);
+    }
+
+}
